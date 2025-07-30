4027ae5502ef83305acb8b165cab7f92f3cacf14
mgurg <34517378+mgurg@users.noreply.github.com>
2025-07-30 15:14:13 +0200

    Nuxt UI setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,36 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/test-utils', '@nuxt/ui']
+  modules: ['@nuxt/eslint', '@nuxt/test-utils', '@nuxt/ui'],
+  css: ['~/assets/css/main.css'],
+  // i18n: {
+  //   strategy: 'prefix_except_default',
+  //   locales: [
+  //     {code: 'pl', iso: 'pl-PL', name: 'Polski', file: 'pl.json'},
+  //     {code: 'en', iso: 'en-US', name: 'English', file: 'en.json'}
+  //   ],
+  //   defaultLocale: 'pl',
+  //   langDir: 'locales',
+  //   vueI18n: './i18n.config.ts'
+  // },
+  router: {
+    options: {
+      linkActiveClass: "active",
+      linkExactActiveClass: "exact-active"
+    }
+  },
+  runtimeConfig: {
+    // Private keys are only available on the server
+    // apiSecret: '123'
+
+    // Public keys that are exposed to the client
+    public: {
+      blogUrl: process.env.NUXT_PUBLIC_BLOG_URL || 'https://default-blog-url.com',
+      baseDomain: process.env.NUXT_PUBLIC_DOMAIN || 'http://localhost:3000',
+      apiBaseUrl: process.env.NUXT_BACKEND_API || 'http://localhost:5000',
+    }
+  },
+  future: {
+    compatibilityVersion: 4,
+  },
 })
