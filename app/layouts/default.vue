b14f8813773bc290c5d1fa56c9fa15c7ccee3abc
mgurg <34517378+mgurg@users.noreply.github.com>
2025-07-30 15:15:05 +0200

    I18n initial setup

---
+++ app/layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex min-h-screen flex-col items-stretch">
+    <HeaderComponent />
+    <div class="grow px-3 sm:px-6"> <!-- Adjusted padding -->
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+main {
+  min-height: calc(100vh - 100px); /* Adjust as needed */
+}
+</style>
