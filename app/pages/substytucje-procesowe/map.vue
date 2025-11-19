e444b0d873e7f48ae60f5e4a1c37f81cf45f039d
mgurg <34517378+mgurg@users.noreply.github.com>
2025-11-19 15:26:42 +0100

    Add Mapbox integration with `nuxt-mapbox`

---
+++ app/pages/substytucje-procesowe/map.vue
@@ -0,0 +1,32 @@
+<template>
+  <UContainer>
+    <UPageSection
+        title="Mapa zleceń prawniczych"
+    >
+      <p class="prose prose-gray max-w-5xl mx-auto">
+        Mapa zleceń prawniczych to intuicyjna platforma prezentująca aktualne oferty substytucji procesowych z całej
+        Polski.
+        <br><br>
+        Przeglądaj dostępne zlecenia na interaktywnej mapie i szybko znajdź sprawy, które odpowiadają Twojej
+        specjalizacji. Idealne narzędzie dla adwokatów i radców prawnych szukających dodatkowych zleceń lub wsparcia w
+        prowadzeniu spraw.
+      </p>
+    </UPageSection>
+    <UPageSection style="height: 600px">
+      <MapboxMap
+          map-id="{ID}"
+          style="position: absolute; top: 0; bottom: 0;width: 100%; height: 100%; z-index: 1;"
+          :options="{
+        style: 'mapbox://styles/mapbox/light-v11',
+        center:[19.0, 52.0],
+        zoom: 6
+      }"
+      />
+
+    </UPageSection>
+  </UContainer>
+</template>
+
+<script setup>
+
+</script>
